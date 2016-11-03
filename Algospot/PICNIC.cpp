0de82d0c0ce308814f2a73283c9253088cc745f4
rnwhdghl12@naver.com <rnwhdghl12@naver.com>
2016-11-04 00:16:23 +0900

    PICNIC 해결

--- Algospot/PICNIC.cpp
+++ Algospot/PICNIC.cpp
@@ -1,18 +1,41 @@
 #include<iostream>
 using namespace std;
+/*
+URL: https://algospot.com/judge/problem/read/PICNIC
+ÇØ°á
+*/
 int pn, cn;
 bool couple[10][10];
-bool check[10]; 
-int Find(){
-	for (int i = 0; i < pn; ++i)
-		;
+bool alone[10]; 
+int Count;
+void Search(int b){
+	if (b >= pn){
+		++Count;
+		return;
+	}
+	if (alone[b]){
+		alone[b] = false;
+		for (int i = 0; i < pn; ++i)
+			if (couple[b][i] && alone[i]){
+				alone[i] = false;
+				Search(b+1);
+				alone[i] = true;
+			}
+		alone[b] = true;
+	}
+	else
+		Search(b + 1);
 }
+
 int main()
 {
 	int Case;
 	cin >> Case;
 	while (Case--){
+		Count = 0;
 		cin >> pn >> cn;
+		for (int i = 0; i < 10; ++i)
+			alone[i] = true;
 		for (int r = 0; r < pn; ++r)
 			for (int c = 0; c < pn; ++c)
 				couple[r][c] = false;
@@ -21,6 +44,8 @@ int main()
 			cin >> b >> e;
 			couple[b][e] = couple[e][b] = true;
 		}
+		Search(0);
+		cout << Count << endl;
 	}
 	return 0;
 }
